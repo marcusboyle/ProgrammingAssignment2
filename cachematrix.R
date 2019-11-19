fb51360b3999747102babad0a0ce09f9099dab71
marcusboyle <marcus.r.boyle@btinternet.com>
2019-11-19 00:27:57 +0000

    Revert "Update cachematrix.R"
    This reverts commit ae91a2e593a4b63ce60f3d482a4fe414d7a19aad.

--- cachematrix.R
+++ cachematrix.R
@@ -8,27 +8,27 @@
 ## functions: getter+setter for the matrix & getter+setter for the
 ## inverse cache. Then returns these 4 functions in a list.
 makeCacheMatrix <- function(x = matrix()) {
-      # Initialise cache to null
-      inv_cache <- NULL
+    # Initialise cache to null
+    inv_cache <- NULL
 
-      ## Matrix functions
-      # Setter: takes a matrix and stores it in "x", resets cache
-      set_mat <- function(mat) {
-            x <<- mat  # "<<-" modifies the variable in the parent function
-            inv_cache <<- NULL
-      }
-      # Getter: returns the matrix "x"
-      get_mat <- function() x
+    ## Matrix functions
+    # Setter: takes a matrix and stores it in "x", resets cache
+    set_mat <- function(mat) {
+        x <<- mat  # "<<-" modifies the variable in the parent function
+        inv_cache <<- NULL
+    }
+    # Getter: returns the matrix "x"
+    get_mat <- function() x
 
-      ## Inverse functions
-      # Setter: takes an answer for inverse and stores it in "inv_cache"
-      set_inv <- function(inv) inv_cache <<- inv
-      # Getter: returns the inverse, which was stored in "inv_cache"
-      get_inv <- function() inv_cache
+    ## Inverse functions
+    # Setter: takes an answer for inverse and stores it in "inv_cache"
+    set_inv <- function(inv) inv_cache <<- inv
+    # Getter: returns the inverse, which was stored in "inv_cache"
+    get_inv <- function() inv_cache
 
-      # Return getters/setters inside a length-4 list
-      list(set_mat=set_mat, get_mat=get_mat,
-           set_inv=set_inv, get_inv=get_inv)
+    # Return getters/setters inside a length-4 list
+    list(set_mat=set_mat, get_mat=get_mat,
+         set_inv=set_inv, get_inv=get_inv)
 }
 
 ## cacheSolve(): Takes the function "makeCacheMatrix()" as input
@@ -38,17 +38,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## been cached, then load the cache -- otherwise get the matrix and
 ## calculate its inverse, which will be cached and returned.
 cacheSolve <- function(x, ...) {
-      # Get the cache of the matrix inverse - return it if it isn't null
-      inv_cache <- x$get_inv()
-      if(!is.null(inv_cache)) {
-            message("Loading inverse from cache")
-            return(inv_cache)
-      }
-      # If the cache is null, then we get and invert the matrix
-      # that was passed into "makeCacheMatrix()"
-      mat <- x$get_mat()
-      inv <- solve(mat, ...)
-      # Store the inverse in the cache and return it
-      x$set_inv(inv)
-      inv
+    # Get the cache of the matrix inverse - return it if it isn't null
+    inv_cache <- x$get_inv()
+    if(!is.null(inv_cache)) {
+        message("Loading inverse from cache")
+        return(inv_cache)
+    }
+    # If the cache is null, then we get and invert the matrix
+    # that was passed into "makeCacheMatrix()"
+    mat <- x$get_mat()
+    inv <- solve(mat, ...)
+    # Store the inverse in the cache and return it
+    x$set_inv(inv)
+    inv
 }
